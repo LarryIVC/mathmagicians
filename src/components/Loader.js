e3ce9223220dae99d5e7cb04f3a55bc27a87de3d
Larry Villegas <larry_villegas@hotmail.com>
2023-05-05 18:57:43 -0400

    Create Loader.js

---
+++ src/components/Loader.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './css/loader.css';
+
+const Loader = () => (
+  <div className="lds-spinner">
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default Loader;
