d0219a5096911c9dd10d0c44e071c9e4a55f8a60
Larry Villegas <larry_villegas@hotmail.com>
2023-05-11 00:34:39 -0400

    Update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -21,39 +21,46 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calc-container">
-      <div className="row">
-        <Display input={state} />
-      </div>
-      <div className="row">
-        <Key manageClick={handleClick}>AC</Key>
-        <Key manageClick={handleClick}>+/-</Key>
-        <Key manageClick={handleClick}>%</Key>
-        <Key manageClick={handleClick}>÷</Key>
-      </div>
-      <div className="row">
-        <Key manageClick={handleClick}>7</Key>
-        <Key manageClick={handleClick}>8</Key>
-        <Key manageClick={handleClick}>9</Key>
-        <Key manageClick={handleClick}>x</Key>
-      </div>
-      <div className="row">
-        <Key manageClick={handleClick}>4</Key>
-        <Key manageClick={handleClick}>5</Key>
-        <Key manageClick={handleClick}>6</Key>
-        <Key manageClick={handleClick}>-</Key>
-      </div>
-      <div className="row">
-        <Key manageClick={handleClick}>1</Key>
-        <Key manageClick={handleClick}>2</Key>
-        <Key manageClick={handleClick}>3</Key>
-        <Key manageClick={handleClick}>+</Key>
-      </div>
-      <div className="row last-row">
-        <Key manageClick={handleClick}>0</Key>
-        <Key manageClick={handleClick}>.</Key>
-        <Key manageClick={handleClick}>=</Key>
-      </div>
+    <div className="main-cont">
+      <article>
+        <h2>Let&apos;s do some math!</h2>
+      </article>
+      <article>
+        <div className="calc-container">
+          <div className="row">
+            <Display input={state} />
+          </div>
+          <div className="row">
+            <Key manageClick={handleClick}>AC</Key>
+            <Key manageClick={handleClick}>+/-</Key>
+            <Key manageClick={handleClick}>%</Key>
+            <Key manageClick={handleClick}>÷</Key>
+          </div>
+          <div className="row">
+            <Key manageClick={handleClick}>7</Key>
+            <Key manageClick={handleClick}>8</Key>
+            <Key manageClick={handleClick}>9</Key>
+            <Key manageClick={handleClick}>x</Key>
+          </div>
+          <div className="row">
+            <Key manageClick={handleClick}>4</Key>
+            <Key manageClick={handleClick}>5</Key>
+            <Key manageClick={handleClick}>6</Key>
+            <Key manageClick={handleClick}>-</Key>
+          </div>
+          <div className="row">
+            <Key manageClick={handleClick}>1</Key>
+            <Key manageClick={handleClick}>2</Key>
+            <Key manageClick={handleClick}>3</Key>
+            <Key manageClick={handleClick}>+</Key>
+          </div>
+          <div className="row last-row">
+            <Key manageClick={handleClick}>0</Key>
+            <Key manageClick={handleClick}>.</Key>
+            <Key manageClick={handleClick}>=</Key>
+          </div>
+        </div>
+      </article>
     </div>
   );
 };
